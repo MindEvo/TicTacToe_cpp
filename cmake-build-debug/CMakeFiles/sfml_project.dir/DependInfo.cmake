88b9dba4ea1f85a2da798adffa99e907347b0113
Thomas <mindevo@outlook.com>
2022-04-03 20:25:58 -0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/sfml_project.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Tommy/CLionProjects/sfml_project/Board.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/Board.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/BoardScorer.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/BoardScorer.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/Game.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/Game.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/GameText.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/GameText.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/Game_Interface.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/Game_Interface.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/Player.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/Player.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/Test.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/Test.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/Thumbnail.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/Thumbnail.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/TicTacToe.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/TicTacToe.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/TicTacToeBoard.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/TicTacToeBoard.cpp.obj"
+  "C:/Users/Tommy/CLionProjects/sfml_project/main.cpp" "C:/Users/Tommy/CLionProjects/sfml_project/cmake-build-debug/CMakeFiles/sfml_project.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/SFML_folder/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sfml_project.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfml_project.dir/Board.cpp.obj"
+  "CMakeFiles/sfml_project.dir/BoardScorer.cpp.obj"
+  "CMakeFiles/sfml_project.dir/Game.cpp.obj"
+  "CMakeFiles/sfml_project.dir/GameText.cpp.obj"
+  "CMakeFiles/sfml_project.dir/Game_Interface.cpp.obj"
+  "CMakeFiles/sfml_project.dir/Player.cpp.obj"
+  "CMakeFiles/sfml_project.dir/Test.cpp.obj"
+  "CMakeFiles/sfml_project.dir/Thumbnail.cpp.obj"
+  "CMakeFiles/sfml_project.dir/TicTacToe.cpp.obj"
+  "CMakeFiles/sfml_project.dir/TicTacToeBoard.cpp.obj"
+  "CMakeFiles/sfml_project.dir/main.cpp.obj"
+  "libsfml_project.dll.a"
+  "sfml_project.exe"
+  "sfml_project.exe.manifest"
+  "sfml_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfml_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
